d82caf1c8bfa46916fa16e02d21f17b979738156
minhtam0412 <minhtam0412@gmail.com>
2019-07-03 14:17:11 +0700

    thêm mới

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-
+	for i := 0; i < 10; i++ {
+		go f(i)
+	}
+	time.Sleep(time.Second * 1)
+	var input string
+	fmt.Scanln(&input)
 }
-
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n,": ", i)
+	}
+}
