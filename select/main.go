0a883b05cbaab736ac76362d6a2cc1168e59448b
minhtam0412 <minhtam0412@gmail.com>
2019-07-03 14:29:41 +0700

    thêm comment code

--- select/main.go
+++ select/main.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
+	//khai báo 2 channel
 	c1 := make(chan string)
 	c2 := make(chan string)
 
 	go func() {
 		for {
+			//gửi dữ liệu vào channel 1
 			c1 <- "from 1"
 			time.Sleep(time.Second * 2)
 		}
@@ -18,6 +20,7 @@ func main() {
 
 	go func() {
 		for {
+			//gủi dữ liêu vào channel 2
 			c2 <- "from 2"
 			time.Sleep(time.Second * 3)
 		}
@@ -25,9 +28,12 @@ func main() {
 
 	go func() {
 		for {
+			//tuỳ ý xử lý tuỳ theo channel
 			select {
+			//xử lý channel 1
 			case msg1 := <-c1:
 				fmt.Println(msg1)
+				//xử lý channel 2
 			case msg2 := <-c2:
 				fmt.Println(msg2)
 			}
